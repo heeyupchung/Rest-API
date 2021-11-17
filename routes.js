ed13cb3eee7948968956bfe0c8b4c78094678eff
heeyupchung <heeyupchung@gmail.com>
2021-11-16 21:01:23 -0800

    create user routes

--- routes.js
+++ routes.js
@@ -14,6 +14,19 @@ router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
   res.status(200).json(user);
 }));
 
-
+// Route that creates a new user.
+router.post('/users', asyncHandler(async (req, res) => {
+  try {
+    await User.create(req.body);
+    res.status(201).json({ "message": "Account successfully created!" });
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+      const errors = error.errors.map(err => err.message);
+      res.status(400).json({ errors });   
+    } else {
+      throw error;
+    }
+  }
+}));
 
 module.exports = router;
